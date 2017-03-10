ad300027d15978a4f938b1ed2df0f7db04b82cfa
Jens Hinkelmann <jens@hinkelmann.ch>
2017-03-10 10:07:46 +0100

    print the different recipes

---
+++ src/components/RecipeLink/RecipeLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router'
+import './RecipeLink.css'
+
+const RecipeLink = props => {
+	const {recipe} = props;
+  return (
+    <Link to={`/recipe/${recipe.id}`} className='recipe-link'>
+      {recipe.name}
+    </Link>
+  )
+}
+
+export default RecipeLink
--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -7,9 +7,9 @@ import './Recipes.css'
 const Recipes = props => {
 	let {recipes} = props;
     if(recipes===undefined){
-        recipes = [{name:"Karotte", url:"/"},{name:"Karotte", url:"/"},{name:"Karotte", url:"/"},{name:"Karotte", url:"/"},{name:"Karotte", url:"/"},{name:"Karotte", url:"/"},{name:"Karotte", url:"/"}]
+        recipes = [{name:"Karotte1", id:"1"},{name:"Karotte2", id:"2"},{name:"Karotte3", id:"3"},{name:"Karotte4", id:"4"},{name:"Karotte5", id:"5"},{name:"Karotte6", id:"6"},{name:"Karotte7", id:"7"}]
     }
-    let linkItems = recipes.map(function(recipe){return [<RecipeLink recipe={recipes[0]}/>]})
+    let linkItems = recipes.map(function(recipe){return [<RecipeLink recipe={recipe}/>]})
   return (
     <div className='recipes-container'>
       <Sidebar
